1d280bc1a7a9acc6fd8aca0f693e2561e7a8d809
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-06-01 17:50:29 -0500

    project coronapp finish

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const hamburgerMenu = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeButton = document.querySelector('.close');
+const listLink = document.querySelectorAll(".menu__list a");
+
 
 function showMenu(){
     menu.classList.toggle('show-menu');
@@ -10,5 +12,12 @@ function hiddenMenu(){
     menu.classList.toggle('show-menu');
 }
 
+function gotosection(){
+    menu.classList.toggle('show-menu');
+} 
+
 hamburgerMenu.addEventListener('click',showMenu);
 closeButton.addEventListener('click',hiddenMenu);
+listLink.forEach(function(link){
+    link.addEventListener("click",gotosection)
+})
